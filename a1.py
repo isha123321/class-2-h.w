127c80d096a15404f5236651c29653baba16ec22
isha123321 <isha101112@hotmail.com>
2024-10-10 15:56:16 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,8 @@
+name = "isha_basharat"
+grade = 7
+percentage = 99.75
+is_girl=True
+print("Name:",name)
+print("data types of name is ",type(name))
+print("Grade:",grade)
+print("data types of grade is ",type(grade))
---
+++ a2.py
@@ -0,0 +1,8 @@
+a=5455
+print("type of a: ",type(a))
+b=99.99
+print("type of b:",type(b))
+c="blacklover"
+print("type of c:",type(c))
+d=False
+print("type of d:",type(d))
